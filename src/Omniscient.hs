527bb2f063dcaf4a8e85ff06bd5096e2a47e2855
Aaron Stevens <bheklilr2@gmail.com>
2016-09-30 22:10:10 -0500

    Now with migrations

--- src/Omniscient.hs
+++ src/Omniscient.hs
@@ -7,9 +7,11 @@ import Control.Monad.Logger (runStderrLoggingT)
 import Database.Persist.Sqlite
 
 import Omniscient.Server
+import Omniscient.Server.Models
 
 defaultMain :: IO ()
 defaultMain = do
-    pool <- runStderrLoggingT $
+    pool <- runStderrLoggingT $ do
         createSqlitePool "omniscient.db" 5
+    runSqlPool (runMigration migrateAll) pool
     runOmniscientServer 8080 pool
